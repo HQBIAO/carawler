bf00865558a9c435f907437850f60084d22d04ef
陈俊标 <junbiao@outlook.com>
2018-12-08 11:02:16 +0800

    test

--- crawler/test.py
+++ crawler/test.py
@@ -1,20 +1,30 @@
-import os
+# coding=utf-8
+import re
 
-try:
-    print(5/0)
-except ZeroDivisionError:
-    print('0000000')
-finally:
-    print('都会执行')
-print("ddd")
+import pandas as pd
 
 
-a_list = [1,2,3]
-print(a_list)
-for a_ in a_list:
-    print(a_)
-print(a_)
-print(a_list)
+def split_ref_item():
+    df_reference = pd.read_csv('/Users/chenjunbiao/project/graduation_project/paper_degree_paper_with_reference.csv')
+    # 创建一个空的 DataFrame
+    ref_item_df = pd.DataFrame(columns=['paper_title', 'paper_id', 'ref_item'])
+
+    for index, row in df_reference.iterrows():
+        if row['id']!=18:
+            continue
+        ref_dict = {'paper_title': row['paper_title'], 'paper_id': row['paper_id']}
+        ref_txt = row['ref_txt']
+        reference_items = re.compile(u'(\\[[1-9][0-9]*\\])').split(ref_txt)
+        reference_items = [item.strip().replace('\n', '') for item in reference_items[1:len(reference_items)] if
+                           len(item) > 5]
+        ref_dict['ref_item'] = reference_items
+        sub_ref_item_df = pd.DataFrame(ref_dict)
+        ref_item_df = pd.concat([ref_item_df, sub_ref_item_df])
+
+def create_df():
+    data = {'a': 8, 'b': [1, 3, 4, 5, 6]}
+    print(pd.DataFrame(data))
 
 
-print(os.listdir('/Users/chenjunbiao/python爬虫培训/crawler'))
+if __name__ == '__main__':
+    create_df()
