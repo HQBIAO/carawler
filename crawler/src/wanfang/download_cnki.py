86f7c7f217bae3d221fe022a38a08370422bb626
陈俊标 <junbiao@outlook.com>
2018-12-19 14:43:24 +0800

    cnki多线程下载

--- crawler/src/wanfang/download_cnki.py
+++ crawler/src/wanfang/download_cnki.py
@@ -17,6 +17,7 @@ logging.basicConfig(filename="cnki.log", level=logging.DEBUG)
 
 class DownCnkiThread(threading.Thread):
     def __init__(self, thread_id):
+        threading.Thread.__init__(self)
         self.thread_id = thread_id
 
     def run(self):
