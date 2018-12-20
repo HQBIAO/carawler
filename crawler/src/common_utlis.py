a78ca026297a5c90d7a2b9698b85729e06345dc3
陈俊标 <junbiao@outlook.com>
2018-12-20 20:50:52 +0800

    过滤没有下载到正确内容的文章

--- crawler/src/common_utlis.py
+++ crawler/src/common_utlis.py
@@ -1,9 +1,27 @@
 # coding=utf-8
 # date:下午1:51
 # author:chenjunbiao
+import glob
 import re
+from pathlib import Path
 
 
 def filter_chi_char(s):
     s = re.findall('[\u4e00-\u9fa5]', s)
     return ''.join(str(e) for e in s)
+
+
+def filter_file_by_size():
+    result = list(Path.home().joinpath('j_ref').rglob("*.caj"))
+    for r in result:
+        size = r.stat().st_size / 1024
+        if size<15:
+            print(r.name, size)
+
+
+if __name__ == '__main__':
+    filter_file_by_size()
+
+    # this can work too
+    # files = glob.glob(str(Path.home().joinpath('j_ref')) + '/**/*.caj', recursive=True)
+    # print(files)
