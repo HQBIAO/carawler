8acfaef7e008d1e424cc5d8d5f21f67920f24515
陈俊标 <junbiao@outlook.com>
2019-02-09 14:04:13 +0800

    保存下载到的外文论文

--- crawler/src/wanfang/download_international.py
+++ crawler/src/wanfang/download_international.py
@@ -2,6 +2,7 @@
 # date:2019/2/1 下午7:45 
 # author:chenjunbiao
 import platform
+from pathlib import Path
 
 import pandas as pd
 import re
@@ -17,6 +18,8 @@ def get_introduction(bs):
     intro_sec = None
     for section in sections:
         h2 = section.find(re.compile('^h[1-6]$'))
+        if not h2:
+            continue
         little_title = h2.text.lower()
         if 'introduction' in little_title:
             intro_sec = section
@@ -38,6 +41,8 @@ def get_conclusion(bs):
     conclu_sec = None
     for section in sections:
         h2 = section.find(re.compile('^h[1-6]$'))
+        if not h2:
+            continue
         little_title = h2.text.lower()
         if 'conclusion' in little_title:
             conclu_sec = section
@@ -64,6 +69,7 @@ def get_en_paper(url, abstract_func, introduction_func, conclusion_func):
         driver = webdriver.Chrome()
     driver.get(url)
     bs = BeautifulSoup(driver.page_source, 'html.parser')
+    driver.close()
     abstract_text = abstract_func(bs)
     introduction_text = introduction_func(bs)
     conclusion_text = conclusion_func(bs)
@@ -74,6 +80,17 @@ if __name__ == '__main__':
     down_df = pd.read_csv('down_df.csv')
     down_en_df = down_df[down_df['res'].notnull()]
     for index, row in down_en_df.iterrows():
+        save_path = Path.home().joinpath('sciencedirect', row['target_paper'], row['ref_title'])
+        if not save_path.exists():
+            save_path.mkdir(parents=True)
+
         if 'sciencedirect' in row['res']:
             content_dict = get_en_paper(row['res'], get_abstract, get_introduction, get_conclusion)
+            for k, v in content_dict.items():
+                if len(v) < 30:
+                    file_name = row['ref_title'] + '&&' + row['uuid'] + '&&' + k + '&&empty.txt'
+                else:
+                    file_name = row['ref_title'] + '&&' + row['uuid'] + '&&' + k + '&&exist.txt'
+                with save_path.joinpath(file_name).open('w') as f:
+                    f.write(v)
             print(content_dict)
